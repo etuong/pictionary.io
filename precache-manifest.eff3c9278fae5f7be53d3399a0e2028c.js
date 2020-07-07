f90eb82b932f0a830bd7c72305f5331fb0a8e412
Ethan Uong <euong.ctr@kr.af.mil>
2020-07-07 18:59:02 -0400

    Updates

--- precache-manifest.eff3c9278fae5f7be53d3399a0e2028c.js
+++ precache-manifest.eff3c9278fae5f7be53d3399a0e2028c.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "5c489e599fc871d7d568",
+    "revision": "9a46573ecc60e776d103",
     "url": "/pictionary.io/css/app.2c3df392.css"
   },
   {
-    "revision": "5c489e599fc871d7d568",
-    "url": "/pictionary.io/js/app.c2e15bd8.js"
+    "revision": "9a46573ecc60e776d103",
+    "url": "/pictionary.io/js/app.63534719.js"
   },
   {
     "revision": "06a3e2b6cc0954638cc7",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/pictionary.io/js/chunk-vendors.77353ea4.js"
   },
   {
-    "revision": "2cdbf26a64ee8a78883b3c5459d6c3f0",
+    "revision": "6461f149ad98956c08c241cd1e765d10",
     "url": "/pictionary.io/index.html"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/pictionary.io/precache-manifest.e6bcc24807e274df0d244a711e77db03.js"
+  "/pictionary.io/precache-manifest.eff3c9278fae5f7be53d3399a0e2028c.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "pictionary.io-client"});

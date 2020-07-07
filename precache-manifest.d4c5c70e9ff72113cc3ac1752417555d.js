517db2922187ef3faba7d57c0066e43c44d8182b
Ethan Uong <euong.ctr@kr.af.mil>
2020-07-07 09:52:30 -0400

    Deploy

---
+++ precache-manifest.d4c5c70e9ff72113cc3ac1752417555d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "773a149e69c1d3740d9c",
+    "url": "/pictionary.io/css/app.2c3df392.css"
+  },
+  {
+    "revision": "773a149e69c1d3740d9c",
+    "url": "/pictionary.io/js/app.36d3d8d5.js"
+  },
+  {
+    "revision": "06a3e2b6cc0954638cc7",
+    "url": "/pictionary.io/css/chunk-vendors.5bae25bc.css"
+  },
+  {
+    "revision": "06a3e2b6cc0954638cc7",
+    "url": "/pictionary.io/js/chunk-vendors.77353ea4.js"
+  },
+  {
+    "revision": "0cbb3e1dc8a38483b0b5c2fcd02445f8",
+    "url": "/pictionary.io/index.html"
+  },
+  {
+    "revision": "a7e27fb43a3536c5fcdfe50c9778a17d",
+    "url": "/pictionary.io/icons/brush.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/pictionary.io/robots.txt"
+  }
+];
